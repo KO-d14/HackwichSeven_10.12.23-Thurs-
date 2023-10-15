a650bba928caaca16bdae0b002533088d4d785de
KO-d14 <ainajk@hawaii.edu>
2023-10-14 15:17:26 -1000

    Initial Commit

---
+++ HackwichSeven_10.12.23(Thurs)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackwichSeven_10.12.23(Thurs)
+//
+//  Created by Aina Kodaira on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackwichSeven_10.12.23(Thurs)UITests/HackwichSeven_10_12_23_Thurs_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HackwichSeven_10_12_23_Thurs_UITestsLaunchTests.swift
+//  HackwichSeven_10.12.23(Thurs)UITests
+//
+//  Created by Aina Kodaira on 10/14/23.
+//
+
+import XCTest
+
+class HackwichSeven_10_12_23_Thurs_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
